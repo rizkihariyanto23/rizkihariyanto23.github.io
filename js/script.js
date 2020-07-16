61699ec0c85313a5d4f024cab90d9c59be8a109e
rizkihariyanto23 <rizki.fela@gmail.com>
2020-07-16 14:37:50 +0700

    membuat paralax, efek animasi pada about dan portfolio

--- js/script.js
+++ js/script.js
@@ -18,3 +18,39 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
+
+// parallax
+$(window).on('load', function(){
+	$('.kiri').addClass('kiri-go');
+	$('.kanan').addClass('kanan-go')
+});
+
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+		'transform': 'translate(0px, '+ wScroll / 4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform': 'translate(0px, '+ wScroll / 2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform': 'translate(0px, '+ wScroll / 1.2 +'%)'
+	});
+
+
+	// portfolio
+	if( wScroll > $('.portfolio').offset().top - 600 ) {
+		
+		$('.portfolio .img-thumbnail').each(function(i) {
+			setTimeout(function() {
+				$('.portfolio .img-thumbnail').eq(i).addClass('muncul');
+			}, 180 * (i+1))
+
+		});
+
+	}
+
+});
